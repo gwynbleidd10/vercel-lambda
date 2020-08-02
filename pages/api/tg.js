29f1cd6146f1a32119aa595c23f5785ed247fc66
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-08-03 00:49:23 +0900

    test

--- pages/api/tg.js
+++ pages/api/tg.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 const User = require('../../models/User')
 
+const TelegramBot = require('node-telegram-bot-api')
+const bot = new TelegramBot(process.env.BOT_TEST, { webHook: true })
+
 module.exports = async (req, res) => {
     if (req.method == 'GET') {
-     
+
         res.json({
             status: 200,
             message: 'WebHook for TG bot',
@@ -16,12 +19,17 @@ module.exports = async (req, res) => {
         //     useCreateIndex: true
         // })
         // let user = await User.findOne({ tg: req.body.message.chat.id })
-        res.json({
-            "method": "sendMessage",
-            "chat_id": req.body.message.chat.id,
-            "reply_to_message_id": req.body.message.message_id,
-            "text": req.body.message
-            //user
+        // res.json({
+        //     "method": "sendMessage",
+        //     "chat_id": req.body.message.chat.id,
+        //     "reply_to_message_id": req.body.message.message_id,
+        //     "text": req.body.message
+        //     //user
+        // })
+        bot.onText('message', (msg) => {
+            //sendMessage(msg.from.id, start(msg.from.id))
+            //checkId(msg.from.id)
+            bot.sendMessage(req.body.message.chat.id, msg)
         })
     }
 }
