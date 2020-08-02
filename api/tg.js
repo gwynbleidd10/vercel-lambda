c2c6371d42040c0ed77460cd52063030a2ba7048
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-08-02 22:51:03 +0900

    tg

---
+++ api/tg.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+    res.json({
+        "method": "sendMessage",
+        "chat_id": req.body.message.chat.id,
+        "reply_to_message_id": req.body.message.message_id,
+        "text": "Привет, Habr!"
+    })
+}
