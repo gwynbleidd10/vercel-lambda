5c55adb99deee1b20570d06c3eb44449cd323a85
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-08-02 23:50:09 +0900

    fix

--- pages/api/tg.js
+++ pages/api/tg.js
@@ -5,6 +5,13 @@ const User = require('../../models/User')
 
 module.exports = async (req, res) => {
     if (req.method == 'GET') {
+     
+        res.json({
+            status: 200,
+            message: 'WebHook for TG bot',
+        })
+    }
+    else {
         await mongoose.connect("mongodb+srv://" + process.env.MDB_USER + ":" + process.env.MDB_PASS + "@" + process.env.MDB_CLUSTER + "/" + process.env.MDB_ESED_DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -12,17 +19,10 @@ module.exports = async (req, res) => {
         })
         let user = await User.findOne({ tg: req.body.message.chat.id })
         res.json({
-            status: 200,
-            message: 'WebHook for TG bot',
-            user: user
-        })
-    }
-    else {
-        res.json({
             "method": "sendMessage",
             "chat_id": req.body.message.chat.id,
             "reply_to_message_id": req.body.message.message_id,
-            "text": "Привет"
+            "text": user
         })
     }
 }
