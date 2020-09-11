70a7e23a1c68065317298b49273cf3c8279385f6
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-09-11 13:31:57 +0900

    Pou

--- pages/api/reports.js
+++ pages/api/reports.js
@@ -11,25 +11,37 @@ module.exports = async (req, res) => {
 
     if (req.query.type == 'esia' || req.query.type == 'smev2' || req.query.type == 'smev3') {
         let data = {
-            env: (req.query.env == 'test') ? 'тестовой' : (req.query.env == 'prod') ? 'промышленной' : 'СРЕДА',
+            env: (req.query.env == 'test') ? 'тестов' : (req.query.env == 'prod') ? 'продуктивн' : 'СРЕД',
             memberFullName: Lists.member[Lists.isystem[req.query.is].member].fullname,
             memberShortName: Lists.member[Lists.isystem[req.query.is].member].shortname,
             memberOgrn: (Lists.member[Lists.isystem[req.query.is].member].ogrn) ? Lists.member[Lists.isystem[req.query.is].member].ogrn : 'ОГРН',
+            memberCode: (Lists.member[Lists.isystem[req.query.is].member].code) ? Lists.member[Lists.isystem[req.query.is].member].code : 'МНЕМОНИКА',
             memberInn: (Lists.member[Lists.isystem[req.query.is].member].inn) ? Lists.member[Lists.isystem[req.query.is].member].inn : 'ИНН',
             isFullName: (Lists.isystem[req.query.is][req.query.type].fullname) ? Lists.isystem[req.query.is][req.query.type].fullname : 'ПОЛНОЕ_НАИМЕНОВАНИЕ_ИС',
             isShortName: (Lists.isystem[req.query.is][req.query.type].shortname) ? Lists.isystem[req.query.is][req.query.type].shortname : 'КРАТКОЕ_НАИМЕНОВАНИЕ_ИС',
+            isCode: (Lists.isystem[req.query.is][req.query.type][req.query.env].code) ? Lists.isystem[req.query.is][req.query.type][req.query.env].code : 'МНЕМОНИКА',
             employeeSurname: (formData.employee) ? Lists.employee[formData.employee].surname : 'ФАМИЛИЯ',
+            employeeSurname2: (formData.employee2) ? Lists.employee[formData.employee2].surname : 'ФАМИЛИЯ2',
             employeeName: (formData.employee) ? Lists.employee[formData.employee].name : 'ИМЯ',
+            employeeName2: (formData.employee2) ? Lists.employee[formData.employee2].name : 'ИМЯ2',
             employeePatronymic: (formData.employee) ? Lists.employee[formData.employee].patronymic : 'ОТЧЕСТВО',
+            employeePatronymic2: (formData.employee2) ? Lists.employee[formData.employee2].patronymic : 'ОТЧЕСТВО2',
+            employeeFio: (formData.employee) ? `${Lists.employee[formData.employee].surname} ${Lists.employee[formData.employee].name} ${Lists.employee[formData.employee].patronymic}` : 'ФИО',
+            employeeFio2: (formData.employee2) ? `${Lists.employee[formData.employee2].surname} ${Lists.employee[formData.employee2].name} ${Lists.employee[formData.employee2].patronymic}` : 'ФИО2',
             employeePost: (formData.employee) ? Lists.employee[formData.employee].post : 'ДОЛЖНОСТЬ',
+            employeePost2: (formData.employee2) ? Lists.employee[formData.employee2].post : 'ДОЛЖНОСТЬ2',
             employeePhone: (formData.employee) ? Lists.employee[formData.employee].phone : 'ТЕЛЕФОН',
+            employeePhone2: (formData.employee2) ? Lists.employee[formData.employee2].phone : 'ТЕЛЕФОН2',
             employeeEmail: (formData.employee) ? Lists.employee[formData.employee].email : 'ПОЧТА',
+            employeeEmail2: (formData.employee2) ? Lists.employee[formData.employee2].email : 'ПОЧТА2',
+            employeeSnils: (formData.employee) ? Lists.employee[formData.employee].snils : 'СНИЛС',
+            employeeSnils2: (formData.employee2) ? Lists.employee[formData.employee2].snils : 'СНИЛС2',
             requestsMin: (formData.requestsmin) ? formData.requestsmin : (Lists.isystem[req.query.is][req.query.type].requestsmin) ? Lists.isystem[req.query.is][req.query.type].requestsmin : 'МИНИМАЛЬНОЕ_КОЛВО_ЗАПРОСОВ',
             requestsMax: (formData.requestsmax) ? formData.requestsmax : (Lists.isystem[req.query.is][req.query.type].requestsmax) ? Lists.isystem[req.query.is][req.query.type].requestsmax : 'МАКСИМАЛЬНОЕ_КОЛВО_ЗАПРОСОВ',
-            signerPost: (formData.signer) ? Lists.signer[formData.signer].post : 'УПОЛНОМОЧЕННОЕ_ЛИЦО'
+            signerPost: (formData.signer) ? Lists.signer[formData.signer].post : 'УПОЛНОМОЧЕННОЕ_ЛИЦО',
         }
-        switch (req.query.type) {            
-            case 'esia':         
+        switch (req.query.type) {
+            case 'esia':
                 data.esiaCode = (Lists.isystem[req.query.is].esia[req.query.env].code) ? Lists.isystem[req.query.is].esia[req.query.env].code : 'МНЕМОНИКА_ЕСИА'
                 data.url = (formData.url) ? formData.url : (Lists.isystem[req.query.is].esia.url) ? Lists.isystem[req.query.is].esia.url : 'URL_ГЛАВНОЙ_СТРАНИЦЫ'
                 data.cert = req.query.is
