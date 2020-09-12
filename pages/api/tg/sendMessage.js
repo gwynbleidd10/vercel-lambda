bc78f622a5351016b68c4d38a710ce99ee3231d6
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-09-12 19:17:18 +0900

    tg

--- pages/api/tg/sendMessage.js
+++ pages/api/tg/sendMessage.js
@@ -1,34 +1,27 @@
 
-let data = {
-    "chat_id": "337277275",
-    "text": "test"
-}
-
 module.exports = async (req, res) => {
+    let message = req.body.message
     let token = '1008172330:AAFR-qVaUe2S1_mcY8x1QxXY6i-AnUGe6DQ'
     let url = `https://api.telegram.org/bot${token}/sendMessage`
 
     if (req.method == 'GET') {
+        res.json({
+            status: 200,
+            message: "Send API",
+        })
+    }
+    else {
         await fetch(url, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(message)
         })
         res.json({
             status: 200,
-            message: "Sending",
-        })
-    }
-    else {
-        res.json({
-            "method": "sendMessage",
-            "chat_id": req.body.message.chat.id,
-            "reply_to_message_id": req.body.message.message_id,
-            "text": req.body.message
-            //user
+            message: "OK",
         })
     }
 }
