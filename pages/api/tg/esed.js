e7e063e505219920c0a15f59164c661152508222
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-09-18 11:51:01 +0900

    tg

--- pages/api/tg/esed.js
+++ pages/api/tg/esed.js
@@ -27,14 +27,15 @@ module.exports = async (req, res) => {
         })
     }
     else {
-        console.log(req.body)
-        res.json({
-            "method": "sendMessage",
-            "chat_id": req.body.message.chat.id,
-            "reply_to_message_id": req.body.message.message_id,
-            "text": req.body.message
-            //user
-        })
+        // console.log(req.body)
+        // res.json({
+        //     "method": "sendMessage",
+        //     "chat_id": req.body.message.chat.id,
+        //     "reply_to_message_id": req.body.message.message_id,
+        //     "text": req.body.message
+        //     //user
+        // })
+        req.json(req.body)
         // bot.onText('message', (msg) => {
         //     //sendMessage(msg.from.id, start(msg.from.id))
         //     //checkId(msg.from.id)
